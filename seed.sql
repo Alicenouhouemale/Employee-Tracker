b20a9d459a9ed117e43b0ee7d8361686262801c7
Alice Nouhouemale <alicenouhouemale@Alices-MacBook-Pro.local>
2022-11-05 10:33:36 -0700

    update

---
+++ seed.sql
@@ -0,0 +1,26 @@
+USE employee_tracker;
+
+INSERT INTO department
+    (name)
+VALUES
+     ("Financing");
+     ("Aministration");
+     ("Management");
+     ("Humain Resource");
+
+INSERT INTO role
+    (title, salary, department_id)
+VALUES
+    ("Marketing Associate", 46000, 3);
+    ("Engineer", 120000, 2);
+    ("Director", 140000, 1);
+    ("Manager", 80000, 4);
+
+INSERT INTO employee    
+    (first_name, last_name, role_id, manager_id)
+VALUES
+    ("Michael", "Johnson", 1);
+    ("Sally", "Mae", 2);
+    ("Andrea", "Ivy", 3);
+    ("Justin", "Kimberlin", );
+    ("Luca", "Brown", 5);
